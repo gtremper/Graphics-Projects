3923bf338a06552851ace4d8d41126536a86b126
Graham Tremper <gtremper@gmail.com>
2012-02-17 16:03:30 -0800

    Fixed transformations

--- Transform.cpp
+++ Transform.cpp
@@ -7,16 +7,14 @@
 //up is always normalized to a length of 1.
 //eye has a length indicating the distance from the viewer to the origin
 
-// Helper rotation function.  Please implement this.  
-
 mat3 Transform::rotate(const float degrees, const vec3& axis) {
 	mat3 parallel( cos(degrees*pi/180.0) );
-	double x = axis[0];
-	double y = axis[1];
-	double z = axis[2];
+	float x = axis[0];
+	float y = axis[1];
+	float z = axis[2];
 	mat3 rotation(x*x, x*y, x*z, x*y, y*y, y*z, x*z, y*z, z*z);
 	rotation = (1-cos(degrees*pi/180.0))*rotation;
-	mat3 cross(0.0, -z, y, z, 0.0, -x, -y, x, 0);
+	mat3 cross(0.0, -z, y, z, 0.0, -x, -y, x, 0.0);
 	cross = sin(degrees*pi/180.0)*cross;
 	mat3 result = parallel + rotation + cross;
 	return result; 
@@ -36,38 +34,38 @@ void Transform::up(float degrees, vec3& eye, vec3& up) {
 }
 
 mat4 Transform::lookAt(const vec3& eye, const vec3& center, const vec3& up) {
-	vec3 a = eye-center;
+	vec3 a = eye-center; //vector from center to eye
 	vec3 w = glm::normalize(a);
-	vec3 u = glm::cross(up,a);
+	vec3 u = glm::cross(up,w);
 	u = glm::normalize(u);
 	vec3 v = glm::cross(w,u);
-	double utrans = -glm::dot(a,u);
-	double vtrans = -glm::dot(a,v);
-	double wtrans = -glm::dot(a,w);
+	float utrans = -glm::dot(eye,u);
+	float vtrans = -glm::dot(eye,v);
+	float wtrans = -glm::dot(eye,w);
 	mat4 M(u[0], u[1], u[2], utrans, v[0], v[1], v[2], vtrans,
 			w[0], w[1], w[2], wtrans, 0, 0, 0, 1);
-	return M; 
+	return M;
 }
 
 mat4 Transform::perspective(float fovy, float aspect, float zNear, float zFar) {
-	//Implement me!!!	
-	mat4 implementMe;
-	return implementMe;
+	float d = 1/tan(pi*fovy/360);
+	float a = -(zFar+zNear)/(zFar-zNear);
+	float b = -(2*zFar*zNear)/(zFar-zNear);
+	mat4 P(d/aspect,0,0,0,0,d,0,0,0,0,a,b,0,0,-1,0);
+	return P;
 }
 
 mat4 Transform::scale(const float &sx, const float &sy, const float &sz) {
-	//Implement me!!!	
-	mat4 implementMe;
-	return implementMe;	
+	mat4 S(sx,0,0,0,0,sy,0,0,0,0,sz,0,0,0,0,1);
+	return S;	
 }
 
 mat4 Transform::translate(const float &tx, const float &ty, const float &tz) {
-	//Implement me!!!
-	mat4 implementMe;
-	return implementMe;	
+	mat4 T(1,0,0,tx,0,1,0,ty,0,0,1,tz,0,0,0,1);
+	return T;	
 }
 
-/* Trash */
+/* Unused constructor and deconstructer */
 Transform::Transform(){}
 Transform::~Transform(){}
 
