4747809106656b26252f70faf62688136ace405e
Wesley Prager <wesleyprager@wesley-pragers-macbook.local>
2012-03-11 23:02:42 -0700

    bitmap texture loader in parser

--- hw3/parser.cpp
+++ hw3/parser.cpp
@@ -387,7 +387,41 @@ void draw(int obj){
 	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
 }
 
+// load a texture into opengl with freeimage
+bool loadTexture(string filename, GLuint &texture) {
+  FIBITMAP *bitmap = NULL;
+  if (!loadBitmap(filename, bitmap)) {
+    return false;
+  }
 
+  // convert to 32 bit bit-depth
+  FIBITMAP *bitmap32 = FreeImage_ConvertTo32Bits(bitmap);
+  FreeImage_Unload(bitmap);
+  if (!bitmap32) {
+    return false;
+  }
+  bitmap = bitmap32;
+
+  // get bits and dimensions
+  BYTE *bits = FreeImage_GetBits(bitmap);
+  int w = FreeImage_GetWidth(bitmap);
+  int h = FreeImage_GetHeight(bitmap);
+
+  // get bit order
+  int order = GL_BGRA;
+
+  // upload texture to opengl
+  glGenTextures(1, &texture);
+  glBindTexture(GL_TEXTURE_2D, texture);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  gluBuild2DMipmaps(GL_TEXTURE_2D, 4, w, h, order, GL_UNSIGNED_BYTE, (GLvoid*)bits);
+
+  // forget our copy of the bitmap now that it's stored the card
+  FreeImage_Unload(bitmap);
+
+  return true;
+}
 
 
 
